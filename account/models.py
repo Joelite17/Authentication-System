c70b5d2d24f4d2a0c51d28841dd04885d2884e6f
Joelite17 <chinedujosiahjkl@gmail.com>
2022-02-05 03:51:50 -0800

    Added README.md

--- account/models.py
+++ account/models.py
@@ -1,5 +1,7 @@
+from math import frexp
 from django.db import models
 from django.contrib.auth.models import User
+# from PIL import Image
 
 
 class Profile(models.Model):
@@ -8,3 +10,10 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    # def save(self, *args, **kawrgs):s
+    #     super().save(*args, **kawrgs)
+    #     img = Image.open(self.image.path)
+    #     if img.height > 300 or img.width > 300:
+    #         output_size = (300, 300)
+    #         img.thumbnail(output_size)
